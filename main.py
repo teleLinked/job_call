bb887e6d32caf94c93d2899c69726f34d7544dc7
MohammadD3veloper <MohammadDeveloperMail@gmail.com>
2024-04-22 21:46:23 +0330

    remove test alerts

--- main.py
+++ main.py
@@ -5,24 +5,7 @@ import alerting.telegram
 
 def main():
     """ main function to run script """
-    alerting.mailer.send_mail(data={
-        'platform': 'test',
-        'title': 'test',
-        'category': 'test',
-        'description': 'test',
-        'location': 'test',
-        'date': 'now'
-    })
-
-    alerting.telegram.send_alert(data={
-        'platform': 'test',
-        'title': 'test',
-        'category': 'test',
-        'description': 'test',
-        'location': 'test',
-        'date': 'now'
-    })
-
+    ...
 
 
 if __name__ == "__main__":
