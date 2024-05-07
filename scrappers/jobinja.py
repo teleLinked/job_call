4755a73b1e20db612caeaf4ac1a7513ba4cc0469
hamidehalaee <hamidehalaeeyasar@gmail.com>
2024-05-07 13:02:59 +0330

    regex

--- scrappers/jobinja.py
+++ scrappers/jobinja.py
@@ -1,7 +1,12 @@
 from playwright.sync_api import sync_playwright,Route,Request, Response
 import csv
 import time
+import re
+li_selector_pattern = '//*[@id="js-jobSearchPaginator"]/div/ul/li[a]/a/text()'
 
+def get_next_page_button(page):
+    next_page_button = page.query_selector(re.sub(r'\[a\]', '[0-9]+', li_selector_pattern))
+    return next_page_button
 
 def insert_into_csv(li_elements):
     with open('list_items.csv', 'a', encoding='utf-8') as csvfile:
@@ -11,7 +16,6 @@ def insert_into_csv(li_elements):
             csv_writer.writerow([li.strip()])  
 
 def get_li_elements(page):
-    # Attempt to locate the elements with a retry mechanism
     for attempt in range(3):  # Retry up to 3 times
         try:
             li_elements = page.query_selector_all('#js-jobSeekerSearchResult > div > div:nth-child(2) > section > div > ul > li')
@@ -21,15 +25,6 @@ def get_li_elements(page):
             print(f"Attempt {attempt+1}: Failed to locate elements - {e}")
             time.sleep(3)  # Wait for 3 seconds before retrying
     return [] 
-
-def on_request(request: Request):
-    pass
-
-def on_response(response: Response):
-    pass
-
-def on_route(route: Route):
-    route.abort()
 
 def scrape_and_insert(url):
    
@@ -43,17 +38,19 @@ def scrape_and_insert(url):
             while True:
                 li_elements = get_li_elements(page)
                 insert_into_csv(li_elements)
-                print("List items inserted into CSV file.")
+                GREEN = '\033[92m'
+                RESET = '\033[0m'
+                print(GREEN + "List items inserted into CSV file."+ RESET)
 
-                next_link = page.query_selector('//*[@id="js-jobSearchPaginator"]/div/ul/li[12]/a')
+                page.wait_for_selector('//*[@id="js-jobSearchPaginator"]/div/ul')
 
-                if not next_link:
-                    break
-
-                next_link.click()
-
-                page.wait_for_load_state('networkidle')
+                dynamic_next = get_next_page_button(page)
+                if dynamic_next:
+                    dynamic_next.click()
+                    page.wait_for_load_state('networkidle')
+                else:
+                    print("No dynamic next link found. Exiting loop.")
+                break
                 
         browser.close()
-                # page.wait_for_selector('//*[@id="js-jobSeekerSearchResult"]/div/div[2]/section/div/ul/li')
-                # page.wait_for_selector('#js-jobSeekerSearchResult > div > div:nth-child(2) > section > div > ul > li',timeout=60000)
+           
